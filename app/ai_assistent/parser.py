42718de68574c7ea2b886862b302f85bb8ea11fa
pozvonish <sherstobitov636@gmail.com>
2024-09-06 22:47:43 +0500

    parse func

--- app/ai_assistent/parser.py
+++ app/ai_assistent/parser.py
@@ -7,21 +7,20 @@ def save_to_file(text, filename='parser_result.txt'):
         file.write(text)
 
 
-url = 'https://briefly.ru/tolstoi/voyna_i_mir_tom_1/#h3-2'
-response = requests.get(url)
+def parse_book(url):
+    response = requests.get(url)
 
+    if response.status_code == 200:
+        soup = BeautifulSoup(response.text, 'html.parser')
 
-if response.status_code == 200:
-    soup = BeautifulSoup(response.text, 'html.parser')
+        paragraphs = soup.find_all('p', class_=False)
+        paragraphs = paragraphs[:-4]
+        
+        all_text = "\n".join([p.get_text() for p in paragraphs])
 
-    paragraphs = soup.find_all('p', class_=False)
-    paragraphs = paragraphs[:-4]
-    
-    all_text = "\n".join([p.get_text() for p in paragraphs])
+        print(all_text)
 
-    print(all_text)
+        save_to_file(all_text, 'p_result.txt')
 
-    save_to_file(all_text, 'p_result.txt')
-
-else:
-    print(f"jopa: {response.status_code}")
+    else:
+        print(f"jopa: {response.status_code}")
