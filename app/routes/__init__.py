c66f4da78eb2ee0165ff35255fd4b28f83090bae
pozvonish <sherstobitov636@gmail.com>
2024-09-08 01:22:12 +0500

    api routes added

--- app/routes/__init__.py
+++ app/routes/__init__.py
@@ -1,8 +1,10 @@
 from flask import Flask
 from .admin import admin
 from .books import books
+from .api import api
 
 
 def register_routes(app: Flask):
     app.register_blueprint(admin, url_prefix="/admin")
     app.register_blueprint(books, url_prefix="/books")
+    app.register_blueprint(api, url_prefix="/api")
---
+++ app/routes/api/__init__.py
@@ -0,0 +1,52 @@
+from app import app, db
+from flask import Blueprint, render_template, request, jsonify
+from app.models import Post, UserFavBook
+from flask_login import current_user
+
+api = Blueprint("api", __name__)
+
+@api.route("/get_posts", methods=["GET"])
+def get_posts():
+    
+    if not request:
+        return jsonify(""), 400
+    
+    posts = Post.query.order_by(Post.created_at.desc()).limit(20).all()
+    print(posts)
+
+    return jsonify({'results': posts}), 200
+
+
+@api.route("/post", methods=['POST'])
+def post():
+
+    if not request:
+        return jsonify(""), 400
+    
+    text = request.form.get('text')
+    image = request.form.get('image_url')
+    user_id = request.form.get('user_id')
+
+    if text is None and image is None:
+        return jsonify(""), 400
+
+    new_post = Post(text=text, image=image, user_id=int(user_id))
+
+    with app.app_context():
+        db.session.add(new_post)
+        db.session.commit()
+
+    return jsonify(""), 200
+
+
+@api.route("/get_fav_books", methods=["GET"])
+def get_fav():
+
+    if not request:
+        return jsonify(""), 400
+    
+    user_id = current_user.id
+
+    favbooks = UserFavBook.query.filter(UserFavBook.id == user_id)
+
+    return jsonify({'results': favbooks}), 200
