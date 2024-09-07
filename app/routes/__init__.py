94fdc5ac8a487528a8f639975b09bf85ea1eb594
dantepalacio <5562707@mail.ru>
2024-09-08 01:17:54 +0500

    feat added chat api

--- app/routes/__init__.py
+++ app/routes/__init__.py
@@ -1,8 +1,12 @@
 from flask import Flask
 from .admin import admin
 from .books import books
-
+from .chat import chat_bp
+from .auth import auth_bp
 
 def register_routes(app: Flask):
     app.register_blueprint(admin, url_prefix="/admin")
     app.register_blueprint(books, url_prefix="/books")
+    app.register_blueprint(chat_bp, url_prefix='/chat')
+    app.register_blueprint(auth_bp)
+    
