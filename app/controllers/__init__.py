41e3650336f764cfeb1229e03e8922ccb434f7c9
Vladislav Atakhanov <eloh1mzee@gmail.com>
2024-09-07 16:22:13 +0500

    feat: add get_book_by_genres

--- app/controllers/__init__.py
+++ app/controllers/__init__.py
@@ -1,25 +1,19 @@
-from os import name
-from app import db, app
-from app.models import User, Book, Genre, GenreBook
+from sqlalchemy import or_
+from app.models import Book, GenreBook
 
-import random
 
-def get_most_popular_books():
-    # books_under_2000 = Book.query.filter(Book.year<=2000)
+def get_most_popular_books() -> list[Book]:
     books_trending = Book.query.order_by(Book.views).limit(10).all()
-    print(books_trending)
+    return books_trending
 
 
-def get_book_by_genres(genres: list):
-    # Book.query.filter(Book.genres)
-    pass
-
-
-
-
-
-
-        
-        
-    # get_books_from_db()
-
+def get_book_by_genres(genres: list[int]) -> list[Book]:
+    books = list(
+        map(
+            lambda g: g.book,
+            GenreBook.query.filter(or_(*(GenreBook.genre_id == i for i in genres)))
+            .limit(20)
+            .all(),
+        )
+    )
+    return books
