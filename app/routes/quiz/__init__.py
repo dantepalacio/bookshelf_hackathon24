7dafed90eed607cd7ad33d3ea80a5043471ca360
Vladislav Atakhanov <eloh1mzee@gmail.com>
2024-09-08 16:47:57 +0500

    fix: quiz header

--- app/routes/quiz/__init__.py
+++ app/routes/quiz/__init__.py
@@ -1,10 +1,9 @@
 from flask import Blueprint, render_template
 
 
+quiz = Blueprint("quiz", __name__)
 
-quiz = Blueprint('quiz', __name__)
 
-
-@quiz.route('/', methods=['GET'])
+@quiz.route("/", methods=["GET"])
 def get_quizz():
-    return render_template('pages/quiz.html')
+    return render_template("pages/quiz.j2")
