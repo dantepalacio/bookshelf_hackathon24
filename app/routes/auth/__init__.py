de46cb33d8d47946ceffebb9bf8756879e2728a3
Vladislav Atakhanov <eloh1mzee@gmail.com>
2024-09-08 11:41:11 +0500

    feat: login and register forms

--- app/routes/auth/__init__.py
+++ app/routes/auth/__init__.py
@@ -1,32 +1,44 @@
-from flask import Blueprint, request, jsonify
+from flask import Blueprint, redirect, render_template, request, jsonify, url_for
 from flask_login import login_user, logout_user, current_user
 from app.models import User
 from werkzeug.security import check_password_hash
 
-auth_bp = Blueprint('auth', __name__)
+auth_bp = Blueprint("auth", __name__)
 
-@auth_bp.route('/api/login', methods=['POST'])
-def api_login():
+
+@auth_bp.route("/login", methods=["POST", "GET"])
+def login():
     if current_user.is_authenticated:
-        return jsonify({"message": "Already logged in"}), 200
+        return redirect(url_for("index"))
 
-    data = request.json
-    email = data.get('email') # type: ignore
-    password = data.get('password') # type: ignore
+    if request.method == "GET":
+        return render_template("pages/login.j2")
 
+    email = request.form.get("email")
+    password = request.form.get("password")
 
     # Ищем пользователя в базе данных
     user = User.query.filter_by(email=email).first()
 
     if user and user.password == password:
         login_user(user)
-        return jsonify({"message": "Login successful"}), 200
+        return redirect(url_for("index"))
     else:
-        return jsonify({"message": "Invalid email or password"}), 401
-    
-@auth_bp.route('/api/logout', methods=['POST'])
-def api_logout():
+        return render_template("pages/login.j2")
+
+
+@auth_bp.route("/logout")
+def logout():
     if current_user.is_authenticated:
         logout_user()
-        return jsonify({"message": "Logout successful"}), 200
-    return jsonify({"message": "Not logged in"}), 400
+        return redirect(url_for("index"))
+    return "", 403
+
+
+@auth_bp.route("/register", methods=["GET", "POST"])
+def register():
+    if current_user.is_authenticated:
+        return redirect(url_for("index"))
+    if request.method == "GET":
+        return render_template("pages/register.j2")
+    return "", 403
